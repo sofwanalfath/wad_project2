9e63891822066b19eea0b581553409b07cb85886
sofwanalfath <116372378+sofwanalfath@users.noreply.github.com>
2023-10-19 14:11:41 +0700

    Add files via upload

---
+++ latihan2.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Latihan 2 </title>
+        <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <body>
+    <form action="latihan3.php" method="get">
+        <h1 style="Color : grey"> SOFWAN ABDUL FATAH </h1>
+        <?php
+        echo "Latihan WAD"
+
+         
+
+         
+
+
+
+
+
+        ?>
+        <form>
+        <div class="mb-3">
+          <label for="input1" class="fxorm-label">Input 1 (angka) </label>
+          <input type="number" name = "input1" class="form-control">
+        </div>
+
+        <div class="mb-3">
+          <p> Pilih Input 2</p>
+        <select class="form-select" aria-label="Default select example">
+            <option selected>Choose</option>
+            <option value="1">+</option>
+            <option value="2">-</option>
+            <option value="3">x</option>
+            <option value="4">/</option>
+        </select>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+
+        
+
+        
+        
+
+            
+
+    </body>
+
+</head>
+</html>
